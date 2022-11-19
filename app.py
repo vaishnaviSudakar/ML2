362dceba2d307e2a2e5273583bc8717280e8fbf7
vaishnaviSudakar <103937074+vaishnaviSudakar@users.noreply.github.com>
2022-11-19 20:28:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ def recommend(movie):
     return recommended_movie_names,recommended_movie_posters
 
 
-st.header('Movie Recommender System')
+st.header('Show Time!')
+st.header('A Movie Recommender System')
 st.subheader('By Vaish ')
 movies = pickle.load(open('movie_list.pkl','rb'))
 similarity = pickle.load(open('similarity.pkl','rb'))
